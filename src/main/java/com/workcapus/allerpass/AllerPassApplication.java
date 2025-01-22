e677e3857e2a9a01306844c785fcad7672016cb0
wookd0ng <wook5307@gmail.com>
2025-01-22 21:53:04 +0900

    feat : Initialize project

---
+++ src/main/java/com/workcapus/allerpass/AllerPassApplication.java
@@ -0,0 +1,13 @@
+package com.workcapus.allerpass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllerPassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllerPassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/workcapus/allerpass/AllerPassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workcapus.allerpass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllerPassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
