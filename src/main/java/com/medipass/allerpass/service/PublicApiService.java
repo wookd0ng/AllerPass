c8f02d6debd3adfa4f5c90d4ae5be26cfc0eddb5
wookd0ng <wook5307@gmail.com>
2025-02-08 17:42:45 +0900

    feat : 병원코드 인증부분 수정(데이터 받아서 telno로 최종 검증하도록)

--- src/main/java/com/medipass/allerpass/service/PublicApiService.java
+++ src/main/java/com/medipass/allerpass/service/PublicApiService.java
@@ -7,7 +7,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 @Service
 public class PublicApiService {
@@ -18,15 +20,41 @@ public class PublicApiService {
     public boolean verifyHospital(String hospitalCode, String hospitalTel){
 
         String apiUrl = "https://apis.data.go.kr/B551182/hospInfoServicev2/getHospBasisList"
-                + "?ServiceKey" + apiKey
-                + "&ykiho" + hospitalCode
-                + "&telno" + hospitalTel;
+                + "?ServiceKey=" + apiKey
+                + "&ykiho=" + hospitalCode
+//                + "&telno" + hospitalTel; telno는 필터링 안되서 제외해도 됨
+                + "&_type=json"; // telno 필터링이 안 되므로 제외
 
         try {
+            // Map 사용 이유 -> 동적으로 데이터 받음
             ResponseEntity<Map> response = restTemplate.getForEntity(apiUrl, Map.class);
-            return response.getBody() != null && !response.getBody().isEmpty();
-        } catch (Exception e){
-            return false;
+            //ResponseEntity에서 데이터를 map<String, object>로 반환해서 형식을 맞춘거고, getBody를 통해서 http 응답 바디를 가져옴
+            Map<String, Object> responseBody = response.getBody();
+
+            if(responseBody == null) return false;
+
+            Map<String, Object> responseMap = (Map<String, Object>) responseBody.get("response");
+            Map<String, Object> bodyMap = (Map<String, Object>) responseMap.get("body");
+            Map<String, Object> itemMap = (Map<String, Object>) bodyMap.get("item");
+
+            if(itemMap == null) return false;
+
+            // Map.get은 반환타입이 Object만 반환해서, Map은 <String, Object> 타입으로 다뤄야하기 때문에
+            // 형변환을 해준거임 그래서 앞에서 Map 선언했는데 또 괄호에 선언한거임
+            List<Map<String,Object>> hospitalList = (List<Map<String,Object>>) itemMap.get("item");
+
+            for (Map<String,Object> hospital : hospitalList){
+                String apiTel = (String) hospital.get("telno");
+
+                if(apiTel != null && apiTel.replace("-","").equals(hospitalTel.replace("-",""))){
+                    return true;
+                }
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
         }
+        return false;
+
     }
 }
