e0ec863959cf9984a4123acc4ae9b13538e6d207
wookd0ng <wook5307@gmail.com>
2025-03-17 23:31:10 +0900

    feat : Redis 설정 파일 추가(RedisConfig, RedisProperties)

---
+++ src/main/java/com/medipass/allerpass/config/redis/RedisProperties.java
@@ -0,0 +1,17 @@
+package com.medipass.allerpass.config.redis;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "spring.data.redis")
+public class RedisProperties {
+
+    private String host;
+    private int port;
+    private String password;
+}
