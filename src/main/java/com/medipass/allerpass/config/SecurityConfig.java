2650c88ce3a58745aa261ec83504c82b13154c9b
wookd0ng <wook5307@gmail.com>
2025-03-23 21:59:00 +0900

    feat : SecurityConfig 허용 API 설정

--- src/main/java/com/medipass/allerpass/config/SecurityConfig.java
+++ src/main/java/com/medipass/allerpass/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.medipass.allerpass.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,17 +8,30 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        List<String> permitUrls = List.of(
+                "/",
+                "/api/hospital/signup",
+                "/api/hospital/login",
+                "/api/hospital/login"
+        );
         http
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll() // ✅ 모든 요청 허용 (로그인 없이 접근 가능)
                 )
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider,permitUrls),UsernamePasswordAuthenticationFilter.class)
                 .csrf(csrf -> csrf.disable()) // ✅ CSRF 비활성화
                 .formLogin(form -> form.disable()) // ✅ 로그인 폼 비활성화
                 .logout(logout -> logout.disable()); // ✅ 로그아웃 기능 비활성화
@@ -29,4 +43,6 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+
 }
